f17833b13d46af071312fa4d4b5bc13f5d2ddc56
Craig Drabik <craig.drabik@txmq.com>
2018-09-18 17:17:30 -0400

    Pointing at updated core

--- hashgraph workspace/hashgraph/source/ExoZooDemo/aviator-core/com/txmq/exo/persistence/BlockLogger.java
+++ hashgraph workspace/hashgraph/source/ExoZooDemo/aviator-core/com/txmq/exo/persistence/BlockLogger.java
@@ -1,11 +1,10 @@
 package com.txmq.exo.persistence;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import com.txmq.exo.messaging.ExoMessage;
+import com.txmq.exo.persistence.nulllogger.NullBlockLogger;
 
 /**
  * BlockLogger is the manager class for Exo's "low rent" blockchain transaction
@@ -25,22 +24,21 @@ public class BlockLogger {
 	 * name to work around the problem.  It should cause minimal overhead when used 
 	 * in a production setting.
 	 */
-	private Map<String, List<IBlockLogger>> loggers = new HashMap<String, List<IBlockLogger>>();
+	private Map<String, IBlockLogger> loggers = new HashMap<String, IBlockLogger>();
 	
 	/**
-	 * Adds a logger to a node and makes it available through a static accessor.
+	 * Setter for assingning a logger to a node and 
+	 * making it available through a static accessor.
 	 */
-	public void addLogger(IBlockLogger logger, String nodeName) {
-		if (!loggers.containsKey(nodeName)) {
-			loggers.put(nodeName, new ArrayList<IBlockLogger>());
-		}
-		loggers.get(nodeName).add(logger);
+	public void setLogger(IBlockLogger logger, String nodeName) {
+		//BlockLogger.logger = logger;
+		loggers.put(nodeName, logger);
 	}
 	
 	/**
 	 * Retrieves the logger associated with the supplied node name
 	 */
-	public List<IBlockLogger> getLoggers(String nodeName) {
+	public IBlockLogger getLogger(String nodeName) {
 		return loggers.get(nodeName);
 	}
 	
@@ -51,16 +49,18 @@ public class BlockLogger {
 	 * automatically registered so execution can procees.
 	 */
 	public void addTransaction(ExoMessage<?> transaction, String nodeName) {
-		for (IBlockLogger logger : this.getLoggers(nodeName)) {
-			logger.addTransaction(transaction);
+		//TODO:  Figure out a way to cut in notification that a transaction has reached consensus and has been processed by the hashgraph.
+		IBlockLogger logger = getLogger(nodeName);
+		if (logger == null) {
+			setLogger(new NullBlockLogger(), nodeName);
+			logger = getLogger(nodeName);
 		}
+		getLogger(nodeName).addTransaction(transaction);
 	}
 	
 	public void flushLoggers() {
-		for (List<IBlockLogger> loggers : this.loggers.values()) {
-			for (IBlockLogger logger : loggers) {
-				logger.flush();
-			}
+		for (String nodeName : this.loggers.keySet()) {
+			this.getLogger(nodeName).flush();
 		}
 	}
 }
