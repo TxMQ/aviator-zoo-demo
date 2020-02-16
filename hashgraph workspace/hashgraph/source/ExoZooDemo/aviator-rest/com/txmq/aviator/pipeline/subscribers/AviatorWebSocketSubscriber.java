cce545a8c1dd67f7d26349ac1ee9e8b891441d18
Craig Drabik <craig.drabik@txmq.com>
2020-02-16 11:40:20 -0500

    Committing what I need so I can back out a bad merge

--- hashgraph workspace/hashgraph/source/ExoZooDemo/aviator-rest/com/txmq/aviator/pipeline/subscribers/AviatorWebSocketSubscriber.java
+++ hashgraph workspace/hashgraph/source/ExoZooDemo/aviator-rest/com/txmq/aviator/pipeline/subscribers/AviatorWebSocketSubscriber.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.txmq.aviator.messaging.AviatorNotification;
 import com.txmq.aviator.pipeline.PipelineStatus;
+import com.txmq.aviator.pipeline.subscribers.AviatorSubscriberBase;
 
 public class AviatorWebSocketSubscriber extends AviatorSubscriberBase<WebSocket> {
 
